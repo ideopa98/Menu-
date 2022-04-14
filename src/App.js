677e8f4abdbca5e3165e80d613e5a54ab2502d49
ideopa98 <ideopa98@gmail.com>
2022-04-14 15:16:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import './TabMenu/tab.css';
+import Tabs from './TabMenu/Tabs';
+
+
+
+const App = () => {
+  return <>
+ <div className="app">
+    <Tabs/>,
+    </div>
+    </>;
+};
+
+export default App;
+
+
+
